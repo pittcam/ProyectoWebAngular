70bcf1d6b9c2434a30c071a48061d310bd9d5e66
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-09-23 18:28:55 -0500

    componentes login y menu

--- AngularFrontend/src/app/app.component.ts
+++ AngularFrontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from "./components/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularFrontend';
+  title = 'AngularTransmilenio';
 }
--- AngularFrontend/src/app/app.routes.ts
+++ AngularFrontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'registrarse', component: RegistrarseComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirigir al login por defecto
+    { path: '**', redirectTo: '/login' }  // Manejo de rutas no encontradas
+];
